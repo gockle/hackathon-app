266f47b3881c3128b24aacb1af179cadd4910637
Kevin Hargita <khargita@amazon.com>
2024-05-23 16:31:51 -0400

    Added input text and button

--- app/src/main/java/com/example/zerohungerhackathon/MainActivity.java
+++ app/src/main/java/com/example/zerohungerhackathon/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.zerohungerhackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import java.io.BufferedReader;
@@ -8,64 +10,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
-    MyAdapter adapter;
-    private final Handler handler = new Handler();
-    private Runnable runnableCode;
-    CsvFileManager csvFileManager;
-
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        csvFileManager = new CsvFileManager(this);
-        List<Item> items = csvFileManager.loadItems();  // Load items
-        adapter = new MyAdapter(this, R.layout.list_item, items, csvFileManager);
-        listView.setAdapter(adapter);
-
-        runnableCode = new Runnable() {
-            @Override
-            public void run() {
-                List<Item> currentItems = readCSV();
-                // Remove the comparison check if you don't want updates to reintroduce removed items
-                adapter.clear();
-                adapter.addAll(currentItems);
-                adapter.notifyDataSetChanged();
-            }
-        };
-        handler.post(runnableCode);
+        editText = findViewById(R.id.editText);
     }
 
-    private List<Item> readCSV() {
-        List<Item> itemList = new ArrayList<>();
-        try {
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(getAssets().open("data.csv")));
-            String line;
-            reader.readLine(); // Skip the header line
-            while ((line = reader.readLine()) != null) {
-                String[] split = line.split(",");
-                if (split.length > 2 && !Boolean.parseBoolean(split[2].trim())) {  // Check if 'used' is false
-                    itemList.add(new Item(split[0],split[1], "false"));
-                }
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void handleButtonPress(View view) {
+        String studentId = editText.getText().toString();
+        Log.d("handleButtonPress", String.format("Tap for studentId: %s", studentId));
+        //TODO: Placeholder to demo alert, replace with logic that handles alert on API response
+        if (studentId.equals("test")){
+            new AlertDialog.Builder(this)
+                    .setTitle("Alert")
+                    .setMessage(String.format("Student (%s) has already received food today.", studentId))
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
-        return itemList;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        handler.removeCallbacks(runnableCode); // Stop the handler when the app is destroyed
     }
 }
